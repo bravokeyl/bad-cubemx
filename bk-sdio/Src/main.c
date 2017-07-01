2cd88f5f14f5e9ed4d28d59ed03baf83f90cf2a6
bravokeyl <bravokeyl@gmail.com>
2017-07-01 15:56:12 +0530

    Enable RCC HSE with crystal/ceramic

--- bk-sdio/Src/main.c
+++ bk-sdio/Src/main.c
@@ -204,6 +204,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
 
